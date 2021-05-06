e7b1b4e87a59009528fe8d0351fa0d285cbdcb74
Aidan Conley <aidancon@udel.edu>
2021-05-06 16:25:23 -0400

    Updates

--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-importScripts("/fish-out-of-water/betaprecache-manifest.593fcb64f784d0d8b92613d0bf56c972.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/fish-out-of-water/betaprecache-manifest.20dd23187ee125d0e4df3738591de66c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
