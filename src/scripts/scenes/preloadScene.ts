0354a90b6ddb977129be93032ed991fa0d603196
Taylor Fields <tvfields@udel.edu>
2021-05-18 09:10:51 -0400

    title scene blank showing up

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -68,7 +68,7 @@ export default class PreloadScene extends Phaser.Scene {
   }
   create() {
     this.add.text(20, 20, "Loading game...");
-    this.scene.start('LevelOneScene');
+    this.scene.start('title');
 
     this.anims.create({
       key: 'clown-idle',
